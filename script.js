1d73dbd7262b573c273d1c4640a09f5f73ab1214
Hakan9092 <hakanekinci.hb@gmail.com>
2021-06-21 22:43:11 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlinder(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
